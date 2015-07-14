eb94bc53219fdf0ba5181a74f6ef67ccac441777
Peter Henry <phenry@caltech.edu>
2015-07-14 11:53:17 -0600

    Added custom context templates in preparation for sidebar

---
+++ lootparser/main/contexts.py
@@ -0,0 +1,9 @@
+from main.models import Paste
+
+
+def pastebar(request):
+    '''Returns a dictionary containing the 8 most recently created pastes.'''
+    pastes = Paste.objects.all().order_by('-created')[:8]
+    context = {}
+    context['pastebar'] = pastes
+    return context
