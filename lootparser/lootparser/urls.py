fad7f10ae4329407f3cb33b343cee4ef43cdca68
Peter Henry <phenry@caltech.edu>
2015-07-15 20:59:18 -0600

    Beginning of login view, template, etc

--- lootparser/lootparser/urls.py
+++ lootparser/lootparser/urls.py
@@ -8,6 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^paste/$', PasteView.as_view(), name='paste'),
     url(r'^paste/(?P<paste_id>\d+)/$', DisplayView.as_view(), name='display'),
 )
--- lootparser/main/views.py
+++ lootparser/main/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, render_to_response, redirect
 from django.template import RequestContext
 from django.views.generic import View
