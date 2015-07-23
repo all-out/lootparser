0e0901b8d2241391fce22b668ee3d7738db1f4b1
Peter Henry <phenry@caltech.edu>
2015-07-23 01:00:19 -0600

    rewrote PasteView to process evepraisal links; DisplayView not helpful yet

--- main/models.py
+++ main/models.py
@@ -4,27 +4,19 @@ from django.db import models
 
 class Paste(models.Model):
     ep_id = models.IntegerField(primary_key=True)
-    ep_link = models.URLField()
-    ep_json = models.TextField()
-    ep_kind = models.CharField(max_length=100)
-    ep_market_id = models.IntegerField()
-    ep_market_name = models.CharField(max_length=100)
-    ep_totals_buy = models.BigIntegerField()
-    ep_totals_sell = models.BigIntegerField()
-    ep_totals_volume = models.BigIntegerField()
+    ep_link = models.URLField(null=True)
+    ep_json = models.TextField(null=True)
+    ep_kind = models.CharField(max_length=100, null=True)
+    ep_market_id = models.IntegerField(null=True)
+    ep_market_name = models.CharField(max_length=100, null=True)
+    ep_totals_buy = models.BigIntegerField(null=True)
+    ep_totals_sell = models.BigIntegerField(null=True)
+    ep_totals_volume = models.BigIntegerField(null=True)
     character = models.ManyToManyField('main.Character')
-    created = models.DateTimeField(editable=False)
-
-    # taken from this discussion of object date stamps:
-    # http://stackoverflow.com/questions/1737017/django-auto-now-and-auto-now-add/1737078#1737078
-    def save(self, *args, **kwargs):
-        '''Set value of created field on first save() call.'''
-        if not self.id:
-            self.created = datetime.datetime.utcnow()
-        return super(Paste, self).save(*args, **kwargs)
+    created = models.DateTimeField(editable=False, null=True)
 
     def __unicode__(self):
-        return 'Paste {}'.format(self.id)
+        return 'Paste {}'.format(self.ep_id)
 
 
 class Character(models.Model):
--- main/views.py
+++ main/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, render_to_response, redirect
 from django.template import RequestContext
 from django.views.generic import View
 from main.models import Paste
-import evepaste
-import json
+import datetime
+import re
+import requests
 
 
 class PasteView(View):
@@ -11,17 +12,60 @@ class PasteView(View):
     def get(self, request, *args, **kwargs):
         context = {}
         return render_to_response(
-            'paste.html', context,
-            context_instance=RequestContext(request))
+                'paste.html', context,
+                context_instance=RequestContext(request))
 
     def post(self, request, *args, **kwargs):
         context = {}
-        raw_paste = request.POST.get('raw_paste', None)
-        new_paste = Paste(raw_paste=raw_paste)
-        parsed = evepaste.parse(raw_paste)
-        new_paste.parsed = json.dumps(parsed)
-        new_paste.save()
-        return redirect('display', paste_id=new_paste.id)
+
+        # get the evepraisal URL from our form
+        ep_link = request.POST.get('ep_link', None)
+
+        # Try to parse form input.  If the regex fails to find satisfactory
+        # link, an AttributeError is caught and the form is reloaded.
+        try:
+
+            # use a regex to get the evepraisal id from the URL and look up
+            # whether this paste already exists in the database
+            regex = r'(?<=\/)\d+$'
+            ep_id = int(re.search(regex, ep_link).group())
+            paste, created = Paste.objects.get_or_create(ep_id=ep_id)
+
+            # if paste has never been seen before, we have fields to populate:
+            if created:
+
+                # populate the paste's ep_link field with the evepraisal URL
+                paste.ep_link = ep_link
+
+                # pull the json from evepraisal's API
+                response = requests.get(ep_link + '.json')
+
+                # format the json response into a python dictionary
+                response_dict = response.json()
+
+                # populate evepraisal fields of the paste object with the dict
+                paste.ep_kind = response_dict['kind']
+                paste.ep_market_id = response_dict['market_id']
+                paste.ep_market_name = response_dict['market_name']
+                paste.ep_totals_buy = response_dict['totals']['buy']
+                paste.ep_totals_sell = response_dict['totals']['sell']
+                paste.ep_totals_volume = response_dict['totals']['volume']
+
+                # get timestamp and convert to datetime field
+                timestamp = response_dict['created']
+                paste.created = datetime.datetime.fromtimestamp(timestamp)
+
+                # save the updated paste object to the database
+                paste.save()
+
+            # redirect to the paste's display page
+            return redirect('display', paste_id=paste.ep_id)
+
+        except AttributeError, e:
+            print e
+            return render_to_response(
+                    'paste.html', context,
+                    context_instance=RequestContext(request))
 
 
 class DisplayView(View):
@@ -29,7 +73,7 @@ class DisplayView(View):
     def get(self, request, *args, **kwargs):
         context = {}
         paste_id = self.kwargs['paste_id']
-        context['paste'] = Paste.objects.get(id=paste_id)
+        context['paste'] = Paste.objects.get(ep_id=paste_id)
         return render_to_response(
                 'display.html', context,
                 context_instance=RequestContext(request))
