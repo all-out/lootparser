c0ee942830edeb8ed0aa287dbf65027603d26fd6
Peter Henry <phenry@caltech.edu>
2015-07-23 23:18:38 -0600

    added blueloot & salvage total value fields. value fields use floats now.

--- main/models.py
+++ main/models.py
@@ -9,11 +9,13 @@ class Paste(models.Model):
     ep_kind = models.CharField(max_length=100, null=True)
     ep_market_id = models.IntegerField(null=True)
     ep_market_name = models.CharField(max_length=100, null=True)
-    ep_totals_buy = models.BigIntegerField(null=True)
-    ep_totals_sell = models.BigIntegerField(null=True)
+    ep_totals_buy = models.FloatField(null=True)
+    ep_totals_sell = models.FloatField(null=True)
     ep_totals_volume = models.BigIntegerField(null=True)
     character = models.ManyToManyField('main.Character')
     created = models.DateTimeField(editable=False, null=True)
+    blue_loot_value = models.FloatField(null=True)
+    salvage_value = models.FloatField(null=True)
 
     def __unicode__(self):
         return 'Paste {}'.format(self.ep_id)
