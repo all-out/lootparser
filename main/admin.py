893fa6d61fe2487e610a874cb4fc4003248ac5a0
Peter Henry <phenry@caltech.edu>
2015-07-26 20:19:09 -0600

    added PasteAdmin for easier paste viewing in admin site

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from main.models import Paste, Op, Character, Tax
 
+
+class PasteAdmin(admin.ModelAdmin):
+    list_display = [
+        'ep_id',
+        'created',
+        'op',
+        'blueloot_value',
+        'salvage_value',
+        'total_value',
+        'ep_totals_volume',
+        'tax',
+    ]
+
+
+admin.site.register(Paste, PasteAdmin)
 admin.site.register(Paste)
 admin.site.register(Character)
 admin.site.register(Op)
